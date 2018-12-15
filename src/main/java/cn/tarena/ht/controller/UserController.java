9e2fcc8f54290e673159c477815c593d3dc2f791
991530643@qq.com <991530643@qq.com>
2018-12-15 15:34:45 +0800

    dddd

--- src/main/java/cn/tarena/ht/controller/UserController.java
+++ src/main/java/cn/tarena/ht/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 import cn.tarena.ht.pojo.UserTable;
 import cn.tarena.ht.service.UserService;
 import cn.tarena.ht.utils.MyTools;
@@ -20,23 +22,62 @@ import cn.tarena.ht.utils.ResultFactory;
 public class UserController {
 	@Autowired
 	private UserService userService;
-	@RequestMapping(value = "/aaa",produces = "application/json;charset=utf-8")
-	public void aaaa(){
-		int a=1;
-		System.out.println(a);
-		
-	}
-	
+
 	/**
 	 * 用户登录
 	 * @author luojiayng
 	 * */
 	@RequestMapping(value = "/login",produces = "application/json;charset=utf-8")
-	public @ResponseBody Result  userLogin(HttpServletRequest request,UserTable u) {
+	public ModelAndView UserController(HttpServletRequest request,UserTable u) {
+		ModelAndView mav=new ModelAndView();
+		String msg="";
+		boolean flag =true;
+		UserTable user = null;
 		if(MyTools.isNullOrEmpty(u.getUname())||MyTools.isNullOrEmpty(u.getUname())){
+			flag = false;
+			msg = "操作数据有误";
+		}else{
+			user=userService.UserLogin(u.getUname());
+			if(user==null){
+				flag = false;
+				msg = "用户不存在";
+			}else{
+				if(!user.getPwd().equals(u.getPwd())){
+					flag = false;
+					msg = "密码错误";
+				}
+				if(user.getStatus()==1){
+					flag = false;
+					msg = "用户已冻结！";
+				}
+			}
+			
+		}
+		if(flag){
+			mav.addObject("userdate", user);
+			mav.setViewName("index");
+		}else{
+			mav.addObject("msg", msg);
+			mav.setViewName("login");
+		}
+		return mav;
+	}
+	
+	/**
+	 * 角色
+	 * @author luojiayng
+	 * */
+	@RequestMapping(value = "/character",produces = "application/json;charset=utf-8")
+	public @ResponseBody Result  Character(HttpServletRequest request,UserTable u) {
+		//参数校验
+		if(u.getUtid()==null||u.getType()>1||u.getType()<0){
 			return ResultFactory.generateResult(ResultConstants.PARAMETER_CODE, 
 					ResultConstants.PARAMETER_MSG);
 		}
+		//老师
+		
+		//学生
+		
 		UserTable user=userService.UserLogin(u.getUname());
 		if(user==null){
 			return ResultFactory.generateResult(ResultConstants.ACCOUNT_NULL_ERROR_CODE, 
